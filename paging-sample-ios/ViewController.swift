2bf0716b2b59026a51ea9edf54426e56d9cfbd2b
kota-ishimoto <kota-ishimoto@cookpad.com>
2020-04-30 15:32:37 +0900

    Initial Commit

---
+++ paging-sample-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  paging-sample-ios
+//
+//  Created by kota-ishimoto on 2020/04/30.
+//  Copyright © 2020 kota-ishimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
